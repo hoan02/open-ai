ae8f0384af31103c3f5ae743454db8ae5a01e72f
hoan02 <hoanvipboi1@gmail.com>
2023-02-18 12:37:55 +0700

    create

---
+++ src/pages/chatgpt/ChatGpt.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ChatGpt.scss";
+
+const ChatGpt = () => {
+  return <div className="chatGpt">ChatGpt</div>;
+};
+
+export default ChatGpt;
---
+++ src/pages/dalle/Dalle.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Dalle.scss";
+
+const Dalle = () => {
+  return <div className="dalle">Dalle</div>;
+};
+
+export default Dalle;
