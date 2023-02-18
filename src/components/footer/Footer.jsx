7f1579507f5cf72163623e3811840ed0483f539c
hoan02 <hoanvipboi1@gmail.com>
2023-02-18 17:39:00 +0700

    create header:navbar and main:video-background

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <h1>1</h1>
+      <h1>1</h1>
+      <h1>1</h1>
+      <h1>1</h1>
+      <h1>1</h1>
+      <h1>1</h1>
+      <h1>1</h1>
+      <h1>1</h1>
+      <h1>1</h1>
+      <h1>1</h1>
+      <h1>1</h1>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import "./Navbar.scss";
+import logo from "../../assets/images/logo.svg";
+import btnMenu from "../../assets/images/btn-menu.png";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <Link to="/">
+          <div className="logo">
+            <img src={logo} alt="logo" className="filter-white" />
+          </div>
+        </Link>
+        <div className="menu-nav">
+          <Link to="/chatgpt" className="link">
+            <span>CHAT GPT</span>
+          </Link>
+          <Link to="/dalle" className="link">
+            <span>DALL-E</span>
+          </Link>
+          <Link to="/about" className="link">
+            <span>ABOUT</span>
+          </Link>
+        </div>
+
+        <div className="menu-nav-mobile">
+          <img src={btnMenu} alt="" className="filter-white" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
